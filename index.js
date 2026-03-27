c6dc3b6ba83e5e88e712778341fb10e860bf28e0
Drha2424 <dwikirichi@gmail.com>
2026-03-27 19:52:13 +0800

    update materi minggu ke 3

--- index.js
+++ index.js
@@ -1,158 +1,254 @@
-import express from 'express'
-import { books, users } from './data.js'
+import express from "express";
+import prisma from "./database.js";
 
-const app = express()
-const port = 3000
-app.use(express.json())
+const app = express();
+const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the API Library')  
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the API Library");
+});
 
 //get
-app.get('/books', (req, res) => {
-    res.send(books)
-})
+app.get("/books", async (req, res) => {
+  const books = await prisma.books.findMany();
+  res.json({
+    success: true,
+    message: "Books retrieved successfully",
+    data: books,
+  });
+});
 
-app.get('/books/:id',(req, res) => {
+app.get("/books/:id", async (req, res) => {
+  //merubah tipe data menjadi integer menggunakan parseInt
+  const id = parseInt(req.params.id);
+  //mencari buku dengan Id yang sesuai menggunakan prisma
+  const book = await prisma.books.findUnique({
+    where: {
+      id: id,
+    },
+  });
 
-    //merubah tipe data menjadi integer menggunakan parseInt
-    const id = parseInt(req.params.id)
-    //mencari buku dengan Id yang sesuai
-    const book = books.find((book) => book.id === id)
-    //jika id buku tidak ditemukan
-    if (!book) {
-        res.send(`Book with ID: ${id} not found !`)
-    }
-
-    res.send(book)
-})
+  //jika id buku tidak ditemukan
+  if (!book) {
+    return res.json({
+      success: false,
+      message: `Book with ID: ${id} not found`,
+    });
+  }
+  res.json({
+    success: true,
+    message: "Book retrieved successfully",
+    data: book,
+  });
+});
 
 // POST
-app.post('/books', (req, res) => {
-    
-    const {title, author, year } = req.body
+app.post("/books", async (req, res) => {
+  const { title, author, year } = req.body;
 
-    const newId = books.length + 1
+  const book = await prisma.books.create({
+    data: {
+      title,
+      author,
+      year,
+    },
+  });
 
-    const newBook = { id: newId, title, author, year}
+  res.json({
+    success: true,
+    message: "Book created successfully",
+    data: book,
+  });
+});
 
-    books.push(newBook)
-
-    res.send('Book created successfully')
- })
-  
 // PUT
-app.put('/books/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+app.put("/books/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const {title, author, year } = req.body
+  const { title, author, year } = req.body;
+  // Mencari buku dengan ID yang sesuai
+  const book = await prisma.books.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  // Jika buku tidak ditemukan, kirimkan pesan error
+  if (!book) {
+    return res.json({
+      success: false,
+      message: `Book with ID: ${id} not found`,
+    });
+  }
 
-    const bookIndex = books.findIndex((book) => book.id === id)
-    
-    if (bookIndex === -1) {
-        res.send(`Book with ID: ${id} not found`)
-        return
-      }
+  // Mengupdate buku dengan ID yang sesuai
+  await prisma.books.update({
+    where: {
+      id: id,
+    },
+    data: {
+      title,
+      author,
+      year,
+    },
+  });
 
-    books[bookIndex] = {
-        id: books[bookIndex].id,
-        title,
-        author,
-        year,
-    }
+  res.json({
+    success: true,
+    message: "Book updated successfully",
+    data: book,
+  });
+});
 
-    res.send(`Book with ID: ${id} updated successfully`)
-})
-  
 // DELETE
-app.delete('/books/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+app.delete("/books/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const bookIndex = books.findIndex((book) => book.id === id)
+  const book = await prisma.books.findUnique({
+    where: {
+      id: id,
+    },
+  });
 
-    if (bookIndex === -1) {
-        res.send(`Book with ID: ${id} not found`)
-        return
-      }
+  if (!book) {
+    return res.json({
+      success: false,
+      message: `Book with ID: ${id} not found`,
+    });
+  }
 
-    books.splice(bookIndex, 1)
-    
-    res.send(`Book with ID: ${id} deleted successfully`)
-})
+  await prisma.books.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  res.json({
+    success: true,
+    message: "Book deleted successfully",
+  });
+});
 
 //get user
-app.get('/users', (req, res) => {
-    res.send(users)
-})
+app.get("/users", async (req, res) => {
+  const users = await prisma.users.findMany();
+  res.json({
+    success: true,
+    message: "Users retrieved successfully",
+    data: users,
+  });
+});
 
-app.get('/users/:id',(req, res) => {
+app.get("/users/:id", async (req, res) => {
+  //merubah tipe data menjadi integer menggunakan parseInt
+  const id = parseInt(req.params.id);
+  //mencari user dengan Id yang sesuai
+  const user = await prisma.users.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  //jika id user tidak ditemukan
+  if (!user) {
+    return res.json({
+      success: false,
+      message: `User with ID: ${id} not found`,
+    });
+  }
 
-    //merubah tipe data menjadi integer menggunakan parseInt
-    const id = parseInt(req.params.id)
-    //mencari user dengan Id yang sesuai
-    const user = users.find((user) => user.id === id)
-    //jika id user tidak ditemukan
-    if (!user) {
-        res.send(`User with ID: ${id} not found !`)
-    }
-
-    res.send(user)
-})
+  res.json({
+    success: true,
+    message: "Users retrieved successfully",
+    data: user,
+  });
+});
 
 // POST User
-app.post('/users', (req, res) => {
-    
-    const {name, email, role } = req.body
+app.post("/users", async (req, res) => {
+  const { name, email, role } = req.body;
 
-    const newId = users.length + 1
+  const user = await prisma.users.create({
+    data: {
+      name,
+      email,
+      role,
+    },
+  });
 
-    const newUser = { id: newId, name, email, role}
+  res.json({
+    success: true,
+    message: "Users created successfully",
+    data: user,
+  });
+});
 
-    users.push(newUser)
-
-    res.send('User created successfully')
- })
-  
 // PUT User
-app.put('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+app.put("/users/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const {name, email, role } = req.body
+  const { name, email, role } = req.body;
 
-    const userIndex = users.findIndex((user) => user.id === id)
-    
-    if (userIndex === -1) {
-        res.send(`User with ID: ${id} not found`)
-        return
-      }
+  const user = await prisma.users.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  // Jika user tidak ditemukan, kirimkan pesan error
+  if (!user) {
+    return res.json({
+      success: false,
+      message: `User with ID: ${id} not found`,
+    });
+  }
 
-      users[userIndex] = {
-        id: users[userIndex].id,
-        name,
-        email,
-        role,
-    }
+  // Mengupdate user dengan ID yang sesuai
+  await prisma.users.update({
+    where: {
+      id: id,
+    },
+    data: {
+      name,
+      email,
+      role,
+    },
+  });
 
-    res.send(`User with ID: ${id} updated successfully`)
-})
-  
+  res.json({
+    success: true,
+    message: "Users updated successfully",
+    data: user,
+  });
+});
+
 // DELETE User
-app.delete('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+app.delete("/users/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const userIndex = users.findIndex((user) => user.id === id)
+  const user = await prisma.users.findUnique({
+    where: {
+      id: id,
+    },
+  });
 
-    if (userIndex === -1) {
-        res.send(`User with ID: ${id} not found`)
-        return
-      }
+  if (!user) {
+    return res.json({
+      success: false,
+      message: `User with ID: ${id} not found`,
+    });
+  }
 
-      users.splice(userIndex, 1)
-    
-    res.send(`User with ID: ${id} deleted successfully`)
-})
+  await prisma.users.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  res.json({
+    success: true,
+    message: "Users deleted successfully",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)  
-})
+  console.log(`Example app listening on port ${port}`);
+});
